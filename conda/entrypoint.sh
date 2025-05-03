3e1d960616a5795825adb0a9918c97b21b0b381f
luisdanielta <luisdaniel.ta@outlook.com>
2025-05-03 09:55:23 +0000

    add: Fix

--- conda/entrypoint.sh
+++ conda/entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 # Copies conda configuration to the expected location
 cp -a /tmp/.condarc /opt/conda
 
+mkdir -p /opt/conda/envs /opt/conda/pkgs
+
 # Fixes permissions for shared environments and package cache
-chmod -R 777 /opt/conda/envs
-mkdir -p /opt/conda/pkgs
-chmod -R 777 /opt/conda/pkgs
+chown -R 1000:1000 /opt/conda/envs /opt/conda/pkgs /opt/conda
+chmod -R 755 /opt/conda/envs /opt/conda/pkgs /opt/conda
 
 exec "$@"
