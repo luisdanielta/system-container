f7d4352ac9435c275aee3159425750b8fc6e89d5
luisdanielta <luisdaniel.ta@outlook.com>
2025-05-05 01:25:22 +0000

    add: Entrypoint logic data

---
+++ user/entrypoint/create-user.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Create group and user
+if ! getent group "${USERNAME}" >/dev/null; then
+  groupadd --gid "${USER_GID}" "${USERNAME}"
+fi
+
+if ! id "${USERNAME}" >/dev/null 2>&1; then
+  useradd \
+    --uid "${USER_UID}" \
+    --gid "${USER_GID}" \
+    --create-home \
+    --shell /bin/bash \
+    "${USERNAME}"
+fi
+
+# Set user password and grant passwordless sudo
+echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/${USERNAME}"
+chmod 0440 /etc/sudoers.d/${USERNAME}
+chown -R ${USER_UID}:${USER_GID} /home/${USERNAME}
+
+#exec /usr/sbin/gosu "${USERNAME}" "$@"
+exec "$@"
